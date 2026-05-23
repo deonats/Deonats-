f55e9e3149eb3aba012c13d092e884ef64a0add1
deonats <dirolka2007@gmail.com>
2026-05-23 11:52:25 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,9 @@
-import sys
-import os
-sys.path.insert(0, os.path.dirname(__file__))
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
 from routes import router
-from db_session import engine
-from models import Base
-
-Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="DEONATS KERNEL", version="1.0.0")
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
+app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"])
 app.include_router(router)
 
 @app.get("/")
